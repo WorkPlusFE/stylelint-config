70edd1a7a9ab695a833f5d69cf96036633160eaf
lwy1010 <im.leewy@gmail.com>
2021-02-18 15:28:12 +0800

    feat: remove sass guidelines config

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-sass-guidelines',
-    'stylelint-config-prettier',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
 
   // 在这里定义你的规则风格，定义规则请为规则提供注释说明
   rules: {
