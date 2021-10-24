7e607cb4c86efd2f244650b398cea82e030574c0
Hejx <531601727@qq.com>
2021-10-24 10:54:53 +0800

    chore: set recommended vue config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-recommended-scss',
+    'stylelint-config-recommended-vue',
     'stylelint-config-prettier',
   ],
 
